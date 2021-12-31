6dc28afc53d4ae36d041b2635a9677005ebec002
zickycoodes <ezekieluduak2@gmail.com>
2021-12-31 19:17:28 +0100

    Understand: the state property in the constructor property is something common to class based components and it stores data in an object to be used at some point in the code. The componentDidMount function in the class is a life cycle method. Life cycle methods are methods peculiar to class based components extended from React.Components. Asides componentDidMount, there are other ones such as: componentDidUpdate & componentWillMount. Again, the content in the componentDidMount could be used directly as it is on the constructor without a doubt.The app I am trying to build is a seasons app that requires the user's current location. This is gotten through a callback passed into and and an error callback, should there be any error. the this.setState function that recieves data from the position and error callback is linked with the this state object in the constructor function in the sense it updates it

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {lat: null, errMessage: ""}
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errMessage: err.message });
+      }
+    );
   }
 
 }
