b2fcbd2353539c00030b0c2e43a872000a112e46
zickycoodes <ezekieluduak2@gmail.com>
2021-12-31 19:45:25 +0100

    On the html file, i added a cdn link to the semantic ui for importingsome css and created a file for my SeasonDisplay component to handle the logic of helping the user ascertain the season of the user's current location. The seasonDisplay takes in - as a prop- the latitiude from the this.state object in the App class and return the user's current season in real time.

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./SeasonDisplay.css"
+
+const seasonsConfig = {
+    winter: {
+        text: "Blur it is chilly",
+        iconName: "snowflake"
+    },
+    summer: {
+        text: "Let us hit the beach",
+        iconName: "sun"
+    }
+}
+
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0? "Summer": "Winter"
+    } else {
+        return lat > 0? "Winter": "Summer"
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const seasons = getSeason(props.lat, new Date().getMonth());
+    // const {text, iconName} = seasonsConfig[seasons]
+    console.log(seasonsConfig[seasons])
+    const text = seasons === "Summer"? "Let us hit the beach": "Blurr! It's chilly";
+    const iconName = seasons === "Summer"? "sun" : "snowflake";
+ 
+    return (
+      <div className = {`season-display ${seasons}`}>
+           <i className = {`massive ${iconName} icon`}></i>
+           <h1>{text}</h1>
+           <i className = {`massive ${iconName} icon`}></i>
+      </div>
+    )
+}
+
+
+
+export default SeasonDisplay;
